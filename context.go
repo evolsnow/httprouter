517a20e7822587a61ab820a4b566595966caa5ee
gsc1215225@gmail.com <github104545>
2015-12-26 22:32:43 +0800

    update existing key

--- context.go
+++ context.go
@@ -2,8 +2,16 @@ package httprouter
 
 //add functions to share values between middlewares
 func (ps *Params) Set(key, value string) {
-	add := Param{Key: key, Value: value}
-	*ps = append(*ps, add)
+	if ps.ByName(key) == "" {
+		add := Param{Key: key, Value: value}
+		*ps = append(*ps, add)
+	} else {
+		for i := range *ps {
+			if (*ps)[i].Key == key {
+				(*ps)[i].Value = value
+			}
+		}
+	}
 }
 
 func (ps Params) Get(key string) string {
