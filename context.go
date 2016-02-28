7f9f94d8ef31429f55b92b196cd94b7ccbc3eb16
evolsnow <gsc1215225@gmail.com>
2016-02-28 18:04:47 +0800

    add userful func

--- context.go
+++ context.go
@@ -1,5 +1,7 @@
 package httprouter
 
+import "strconv"
+
 //add functions to share values between middlewares
 func (ps *Params) Set(key, value string) {
 	if ps.ByName(key) == "" {
@@ -16,4 +18,12 @@ func (ps *Params) Set(key, value string) {
 
 func (ps Params) Get(key string) string {
 	return ps.ByName(key)
+}
+
+func (ps Params)GetInt(key string) (value int) {
+	value, err := strconv.Atoi(ps.ByName(key))
+	if err != nil {
+		return 0
+	}
+	return
 }
